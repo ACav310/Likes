a84ddf94529874e6aa5a878cfd43ccabc547b317
acav310 <acavanaugh2013@gmail.com>
2022-03-29 14:56:07 -0400

    initialBuild

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+function like() {
+  var likes = document.getElementById('number').innerText;
+  likes++;
+  document.getElementById('number').innerText = likes;
+}
+
+function like1() {
+  var likes = document.getElementById('number1').innerText;
+  likes++;
+  document.getElementById('number1').innerText = likes;
+}
+
+function like2() {
+  var likes = document.getElementById('number2').innerText;
+  likes++;
+  document.getElementById('number2').innerText = likes;
+}
+
